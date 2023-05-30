1d90ab941a7c5c610e00fe11983c0bef8d8d0239
12ArchanaA <101241089+12ArchanaA@users.noreply.github.com>
2023-05-30 18:50:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,444 @@
+import logo from "./logo.svg";
+import "./App.css";
+import saveImg from "./save.svg";
+import approvalImg from "./approvals.svg";
+import previewImg from "./preview.svg";
+import pasteImg from "./paste.svg";
+import pullImg from "./pull.svg";
+import publishImg from "./publish.svg";
+import deleteImg from "./delete.svg";
+import copyImg from "./copy.svg";
+import vectorImg from "./Vector.svg";
+import qualityImg from "./quality.svg";
+import notificationImage from "./notification.svg";
+import accountImage from "./account.svg";
+import nextImg from "./next.svg";
+import headphoneImg from "./headphone.svg";
+import rectangleImg from "./Rectangle.svg";
+import rectangleScrollImg from './RectangleScroll.svg';
+import checkboxSquareImg from './checkboxSquare.svg';
+import arrowExpandImg from './ExpandArrow.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="left-side">
+          <div className="blue">
+            <div className="left-heading">
+              <img src={logo}></img>
+            </div>
+            <div className="left-heading-content">
+              <div className="image-name">
+                <img src={saveImg}></img>
+                <h5>Save</h5>
+              </div>
+              <div className="image-name">
+                <img src={approvalImg}></img>
+                <h5>Approve</h5>
+              </div>
+              <div className="image-name">
+                <img src={publishImg}></img>
+                <h5>Publish</h5>
+              </div>
+              <div className="image-name">
+                <img src={pullImg}></img>
+                <h5>Pull</h5>
+              </div>
+              <div className="image-name">
+                <img src={previewImg}></img>
+                <h5>Preview</h5>
+              </div>
+              <div className="image-name">
+                <img src={copyImg}></img>
+                <h5>Copy</h5>
+              </div>
+              <div className="image-name">
+                <img src={pasteImg}></img>
+                <h5>Paste</h5>
+              </div>
+              <div className="image-name">
+                <img src={deleteImg}></img>
+                <h5>Delete</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="right-side">
+          <div className="right-side-heading">
+            <div className="path">
+              <div className="path-image">
+                <div>
+                  <img src={vectorImg}></img>
+                </div>
+                <div>
+                  <p>
+                    Projects {">"} FutureIK {">"} Cinemas Campaigns
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="logo">
+              <img src={qualityImg}></img>
+              <img src={notificationImage}></img>
+              <img src={accountImage}></img>
+            </div>
+          </div>
+          <div className="right-side-body">
+            <div className="white-body">
+              <div className="white-body-header">
+                <div className="button-set1">
+                  <button className="blue-button1" type="button">
+                    WHAT
+                  </button>
+                  <button className="button-set1-button" type="button">WHEN</button>
+                  <button className="button-set1-button" type="button">WHERE</button>
+                  <button className="button-set1-button" type="button">SETTINGS</button>
+                  <button className="button-set1-button" type="button">HISTORY</button>
+                </div>
+                <div className="button-set2">
+                  <button type="button">+ Add Media</button>
+                </div>
+                <div className="button-set3">
+                  <button type="button">+ Add Zone</button>
+                </div>
+              </div>
+              <div className="white-body-content">
+                <div className="content1">
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>
+                          <img src={rectangleImg} />
+                        </th>
+                        <th>Order</th>
+                        <th>Name</th>
+                        <th>Type</th>
+                        <th>...</th>
+                      </tr>
+                    </thead>
+                    <tbody className="table-body">
+                      <tr>
+                        <td>
+                          <img src={rectangleImg} />
+                        </td>
+                        <td>1</td>
+                        <td>FutureIK sample ticker</td>
+                        <td>Ticker</td>
+                        <td>...</td>
+                      </tr>
+
+                      <tr>
+                        <td>
+                          <img src={rectangleImg} />
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+
+                      <tr>
+                        <td>
+                          <img src={rectangleImg} />
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img src={rectangleImg} />
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img src={rectangleImg} />
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img src={rectangleImg} />
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+
+                      <tr>
+                        <td>
+                          <img src={rectangleImg} />
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img src={rectangleImg} />
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img src={rectangleImg} />
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img src={rectangleImg} />
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                     
+                      <tr>
+                        <td>
+                          <img src={rectangleImg} />
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img src={rectangleImg} />
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img src={rectangleImg} />
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img src={rectangleImg} />
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img src={rectangleImg} />
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                      </tr>
+                      
+                    </tbody>
+                  </table>
+                </div>
+                <div className="content2">
+                  <div className="content2-arrow">
+                    <img src={nextImg} />
+                  </div>
+                  <div className="content2-body">
+                    <div className="content">
+                      <div className="content-heading">
+                        <div>
+                          <h4>Content</h4>
+                        </div>
+                        <div>
+                          <button className="blue-button">Automatic</button>
+                          <button className="static-button">Static</button>
+                        </div>
+                        <div className="add-ticker-button">
+                          <button>+ Add Ticker</button>
+                        </div>
+                      </div>
+                      <div className="content-body">
+                        <table>
+                          <tr>
+                            <td><img src={rectangleImg}/></td>
+                            <td>http://service.futureik.com/sample/testticker.rss</td>
+                          </tr>
+                          <tr>
+                            <td><img src={rectangleImg}/></td>
+                            <td></td>
+                          </tr>
+                          <tr>
+                            <td><img src={rectangleImg}/></td>
+                            <td></td>
+                          </tr>
+                          <tr>
+                            <td><img src={rectangleImg}/></td>
+                            <td></td>
+                          </tr>
+                          <tr>
+                            <td><img src={rectangleImg}/></td>
+                            <td></td>
+                          </tr>
+                          <tr>
+                            <td><img src={rectangleImg}/></td>
+                            <td></td>
+                          </tr>
+                        </table>
+                      </div>
+                    </div>
+                    <div className="ticker-property">
+                      <h4>Ticker Property</h4>
+                      <div className="ticker-property-content">
+                        <div className="width-height">
+                          <div className="width-bar">
+                            <p>width:</p>
+                            <img src={rectangleScrollImg}/>
+                          </div>
+                          <div className="height-bar">
+                            <p>height:</p>
+                            <img src={rectangleScrollImg}/>
+                          </div>
+                        </div>
+                        <div className="scroll-speed-bar">
+                          <p>Scroll Speed</p>
+                          <img src={rectangleScrollImg}/>
+                        </div>
+
+                        <div className="refresh-rate">
+                          <p>Refresh Rate(Min)</p>
+                          <input type="text" placeholder="15"></input>
+                        </div>
+                        <div className="item-display-duration">
+                          <p>Item Display Duration (Secs)</p>
+                          <input type="text" placeholder="10"></input>
+                        </div>
+                        <div className="text-style">
+                          <input type="text" placeholder="Proxima Nova"></input>
+                        </div>
+                        <div className="text-type">
+                          <div className="text-type-box">
+                            <div><p>Regular</p></div>
+                            <div><img src={arrowExpandImg}/></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="content3">
+                  <div className="resolution">
+                    <div className="row1">
+                      <div>
+                        <p>Total Resolution (px)</p>
+                      </div>
+                      <div className="width">
+                        <label>W</label>
+                        <input type="text" placeholder="Width"></input>
+                      </div>
+                      <div className="height">
+                        <label>H</label>
+                        <input type="text" placeholder="Height"></input>
+                      </div>
+                    </div>
+                    <div className="row2">
+                      <div>
+                        <p>Position (px)</p>
+                      </div>
+                      <div className="resolution-x">
+                        <label>X</label>
+                        <input type="text" placeholder="Width"></input>
+                      </div>
+                      <div className="y">
+                        <label>Y</label>
+                        <input type="text" placeholder="Height"></input>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="box">
+                    <div className="gray"></div>
+                    <div className="writing"></div>
+                  </div>
+                  <div className="property">
+                    <div className="zone">
+                      <div className="selected-zone">
+                        <label>Selected Zone</label>
+                        <input type=""></input>
+                      </div>
+                      <div className="units">
+                        <label>Units</label>
+                        <input type=""></input>
+                      </div>
+                    </div>
+                    <div className="properties">
+                      <h4>Properties</h4>
+                      <div className="properties-content">
+                        <div className="properties-content-row1">
+                          <div>
+                            <p>Resolution: (%)</p>
+                          </div>
+                          <div className="width">
+                            <label>W</label>
+                            <input type="text" placeholder="Width"></input>
+                          </div>
+                          <div className="height">
+                            <label>H</label>
+                            <input type="text" placeholder="Height"></input>
+                          </div>
+                        </div>
+                        <div className="properties-content-row2">
+                          <div>
+                            <p>Position: (%) </p>
+                          </div>
+                          <div className="x">
+                            <label>X</label>
+                            <input type="text" placeholder="Width"></input>
+                          </div>
+                          <div className="y">
+                            <label>Y</label>
+                            <input type="text" placeholder="Height"></input>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="lock">
+                      <label>Lock</label>
+                      <img src={checkboxSquareImg}/>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <img className="headphone" src={headphoneImg} />
+          <div className="right-side-footer">
+            <h6>Copyright@2023.FutureIK </h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
